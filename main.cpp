32aa1f2c1c80fd7ba1526d8e22cfe0c25c0cf2b7
etsub <etsubtayeelon@outlook.com>
2023-04-21 15:16:34 +0300

    Lib commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include "iostream"
-
+#include "vector"
+#include "algorithm"
 using  namespace std ;
+struct Process{
+    int Process_Id;
+    int Arrival_Time;
+    int Burst_Time;
+    int Waiting_Time;
+    int Turn_Around_Time;
+    int rt;
 
+};
 int main (){
     cout<<"Hello World" <<endl;
-
     return  0;
 
 }

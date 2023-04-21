7434f9db7226e620dd73fc49bf5df3b5d70cf5da
etsub <etsubtayeelon@outlook.com>
2023-04-21 15:59:26 +0300

    menu commit

--- main.cpp
+++ main.cpp
@@ -11,11 +11,34 @@ struct Process{
     int Remaining_Time ;
 
 };
-void welcome (){
+void main_menu(){
+ cout<<"Main Menu";
 
 }
+void welcome (){
+    cout << "Welcome to the OS Scheduling Algorithms Model |-- O.S.A.M -- | !\n";
+    int choice;
+    do {
+    cout << "Please select an option:\n";
+    cout << "1. Continue\n";
+    cout << "2. Exit\n";
+    cin >> choice;
+    switch (choice){
+        case 1:
+            main_menu();
+            break;
+        case 2:
+            cout <<"Exiting O.S.A.M ...."<<endl;
+            exit(0);
+        default:
+            cout<<"Invalid choice . Please try again ." << endl;
+    }
+
+    } while (choice != 1);
+
+}
+
 int main (){
-    cout<<"Hello World" <<endl;
-    return  0;
+   welcome();
 
 }

c079fe63032327cce72aca4a87ac919897b5935b
etsub <etsubtayeelon@outlook.com>
2023-04-21 16:07:21 +0300

    menu commit

--- main.cpp
+++ main.cpp
@@ -11,10 +11,48 @@ struct Process{
     int Remaining_Time ;
 
 };
-void main_menu(){
- cout<<"Main Menu";
+void displayMenu() {
+    int choice;
 
+    while (true) {
+        cout << "Main Menu" << endl;
+        cout << "---------" << endl;
+        cout << "1. FCFS" << endl;
+        cout << "2. SJF_NP" << endl;
+        cout << "3. SJF_P" << endl;
+        cout << "4. Round Robin" << endl;
+        cout << "5. Priority" << endl;
+        cout << "6. Exit" << endl;
+
+        cout << "Enter your choice (1-6): ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                // Call function for FCFS algorithm
+                break;
+            case 2:
+                // Call function for SJF_NP algorithm
+                break;
+            case 3:
+                // Call function for SJF_P algorithm
+                break;
+            case 4:
+                // Call function for Round Robin algorithm
+                break;
+            case 5:
+                // Call function for Priority algorithm
+                break;
+            case 6:
+                // Exit program
+                exit(0);
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+                break;
+        }
+    }
 }
+
 void welcome (){
     cout << "Welcome to the OS Scheduling Algorithms Model |-- O.S.A.M -- | !\n";
     int choice;
@@ -25,7 +63,7 @@ void welcome (){
     cin >> choice;
     switch (choice){
         case 1:
-            main_menu();
+            displayMenu();
             break;
         case 2:
             cout <<"Exiting O.S.A.M ...."<<endl;
